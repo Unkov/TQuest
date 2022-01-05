5bc8402cd397dc63cc54af5a92acefa161d243d8
kolya112 <nikolayurch@gmail.com>
2022-01-05 18:08:00 +0300

    Добавьте файлы проекта.

---
+++ TQuest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using INIManager;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace TQuest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
