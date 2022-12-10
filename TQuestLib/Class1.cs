c36d8a1f34a5e187a960e953ce0234bf040952f0
kolya112 <nikolayurch@gmail.com>
2022-12-10 15:04:36 +0300

    TQuestLib: перенесёна функция Core.MissionImpossible() из TQuest, изменено авторство в Core.intro(), добавлен класс Internet с методами для проверки соединения с интернетом
    TQuest: добавлена проверка на актуальность версии игры при наличии интернета у пользователя, исправлено

--- TQuestLib/Class1.cs
+++ TQuestLib/Class1.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 
 namespace TQuestLib
 {
     public class Core
     {
-        public static void intro()
+        public static void intro() // Текст в начале игры
         {
             Console.WriteLine("     █████   █    ██ ▓█████   ██████ ▄▄▄█████▓");
             Console.WriteLine("   ▒██▓  ██▒ ██  ▓██▒▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒");
@@ -18,7 +23,7 @@ namespace TQuestLib
             Console.WriteLine("      ░   ░  ░░░ ░ ░    ░   ░  ░  ░    ░      ");
             Console.WriteLine("       ░       ░        ░  ░      ░           ");
             Console.WriteLine("                                              ");
-            Console.WriteLine("     Developed by MineCR & Nikolay Yurchenko github.com/kolya112/TQuest");
+            Console.WriteLine("     Unkov Company | www.unkov.cf | vk.com/unkovcompany");
         }
 
         public static void print(string text, ConsoleColor color = ConsoleColor.White, ConsoleColor color2 = ConsoleColor.White)
@@ -35,7 +40,7 @@ namespace TQuestLib
             Console.ForegroundColor = color2;
         }
 
-        public static string sha256hash(string text)
+        public static string sha256hash(string text) // Шифрование ключа сейфа
         {
             StringBuilder oResHash = new StringBuilder();
 
@@ -49,6 +54,109 @@ namespace TQuestLib
             }
 
             return oResHash.ToString();
+        }
+
+        public static void MissionImpossible() // Музыка в начале игры
+        {
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+        }
+    }
+
+    public static class Internet
+    {
+        [DllImport("wininet.dll")]
+        static extern bool InternetGetConnectedState(ref InternetConnectionState lpdwFlags, int dwReserved);
+
+        [Flags]
+        enum InternetConnectionState : int
+        {
+            INTERNET_CONNECTION_MODEM = 0x1,
+            INTERNET_CONNECTION_LAN = 0x2,
+            INTERNET_CONNECTION_PROXY = 0x4,
+            INTERNET_RAS_INSTALLED = 0x10,
+            INTERNET_CONNECTION_OFFLINE = 0x20,
+            INTERNET_CONNECTION_CONFIGURED = 0x40
+        }
+
+        static object _syncObj = new object();
+
+        /// <summary>
+        /// Проверить, есть ли соединение с интернетом
+        /// </summary>
+        /// <returns></returns>
+        public static Boolean CheckConnection()
+        {
+            lock (_syncObj)
+            {
+                try
+                {
+                    InternetConnectionState flags = InternetConnectionState.INTERNET_CONNECTION_CONFIGURED | 0;
+                    bool checkStatus = InternetGetConnectedState(ref flags, 0);
+
+                    if (checkStatus)
+                        return PingServer(new string[]
+                                            {
+                                                @"google.com",
+                                                @"microsoft.com",
+                                                @"ibm.com"
+                                            });
+
+                    return checkStatus;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Пингует сервера, при первом получении ответа от любого сервера возвращает true 
+        /// </summary>
+        /// <param name="serverList">Список серверов</param>
+        /// <returns></returns>
+        public static bool PingServer(string[] serverList)
+        {
+            bool haveAnInternetConnection = false;
+            Ping ping = new Ping();
+            for (int i = 0; i < serverList.Length; i++)
+            {
+                PingReply pingReply = ping.Send(serverList[i]);
+                haveAnInternetConnection = (pingReply.Status == IPStatus.Success);
+                if (haveAnInternetConnection)
+                    break;
+            }
+
+            return haveAnInternetConnection;
         }
     }
 }
